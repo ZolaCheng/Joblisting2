c96cc5f4a6314ba731b9c49a5de5fce998d86e0a
Zola Chen <zolacheng@gmail.com>
2017-06-22 12:24:45 +0800

    implement part 2

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,6 +1,6 @@
-class JobsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :update, :edite, :destory]
-
+class Admin::JobsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_action :require_is_admin
 
   def show
     @job = Job.find(params[:id])
@@ -18,25 +18,30 @@ class JobsController < ApplicationController
     @job = Job.new(job_params)
 
     if @job.save
-      redirect_to jobs_path
+      redirect_to admin_jobs_path
     else
       render :new
     end
   end
 
   def edit
-    @job = Job.find(params[:id])
+    @job = Jop.find(params[:id])
   end
 
   def update
     @job = Job.find(params[:id])
     if @job.update(job_params)
-      redirect_to jobs_path
+      redirect_to admin_job_path
     else
       render :edit
     end
   end
 
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to admin_jobs_path
+  end
 
   private
 
@@ -44,5 +49,6 @@ class JobsController < ApplicationController
     params.require(:job).permit(:title, :description)
   end
 
+  
 
 end
